524b502916e2dafbef3f5737da6d553879281a6f
chamila107 <chamila.m@eyepax.com>
2018-05-11 18:47:06 +0530

    Added new WPF project

---
+++ MillenniumERPWPF/MillenniumERP/ViewModels/AddressBookViewModel.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AddressBookViewModel.cs" company="Listensoftware">
+//     Copyright (c) Listensoftware Pte. Ltd. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace MillenniumERP.ViewModels
+{
+    using Caliburn.Micro;
+
+    /// <summary>
+    /// Interaction logic for AddressBookViewModel
+    /// </summary>
+    /// <seealso cref="Caliburn.Micro.Screen" />
+    public class AddressBookViewModel : Screen
+    {
+
+        /// <summary>
+        /// Do the searching
+        /// </summary>
+        public void Search()
+        {
+
+
+        }
+    }
+}
---
+++ MillenniumERPWPF/MillenniumERP/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MainViewModel.cs" company="Listensoftware">
+//     Copyright (c) Listensoftware Pte. Ltd. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace MillenniumERP.ViewModels
+{
+    using Caliburn.Micro;
+    /// <summary>
+    /// Interaction logic for MainViewModel
+    /// </summary>
+    /// <seealso cref="Caliburn.Micro.Screen" />
+    public class MainViewModel : Conductor<object>
+    {
+        /// <summary>
+        /// Loads the address book.
+        /// </summary>
+        public void LoadAddressBook()
+        {
+            ActivateItem(new AddressBookViewModel());
+        }
+    }
+}
