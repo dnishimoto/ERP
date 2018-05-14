32b15ab01028d18eda493ea1c18e3f50ffb1e026
David Nishimoto <dnishimoto@listensoftware.com>
2018-05-14 12:58:19 -0600

    Moved repositories to ERP_Core

--- AddressBook/ERP_Core2/EntityFramework/ERPDataModel.Designer.cs
+++ AddressBook/ERP_Core2/EntityFramework/ERPDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Owner\Source\Repos\MillenniumERP\AddressBook\MillenniumERP\EntityFramework\ERPDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Owner\Source\Repos\MillenniumERP\AddressBook\ERP_Core2\EntityFramework\ERPDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
