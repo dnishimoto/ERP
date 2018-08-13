184b1c5c1328603b4b0518bc55a3e3f80e0394d0
David Nishimoto <dnishimoto@listensoftware.com>
2018-08-12 22:40:26 -0600

    time and attendance module

---
+++ AddressBook/ERP_Core2/TimeAndAttendanceDomain/TimeAndAttendanceModule.cs
@@ -0,0 +1,76 @@
+﻿using ERP_Core2.AbstractFactory;
+using ERP_Core2.EntityFramework;
+using MillenniumERP.ScheduleEventsDomain;
+using MillenniumERP.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP_Core2.TimeAndAttendanceDomain
+{
+    class TimeAndAttendanceModule : AbstractModule
+    {
+        UnitOfWork unitOfWork = new UnitOfWork();
+
+        public async Task<long> AddPunchIn(TimeAndAttendancePunchIn taPunchin)
+        {
+
+            try
+            {
+                long timePunchinId = await unitOfWork.TARepository.AddPunchin(taPunchin);
+                unitOfWork.CommitChanges();
+
+                return timePunchinId;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(GetMyMethodName(), ex);
+            }
+        }
+        public async Task<TimeAndAttendancePunchIn> GetPunchInById(long timePunchinId)
+        {
+            try
+            {
+
+                TimeAndAttendancePunchIn taPunchin = await unitOfWork.TARepository.GetObjectAsync(timePunchinId);
+                return taPunchin;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(GetMyMethodName(), ex);
+            }
+        }
+        public IList<TimeAndAttendancePunchInView> GetTAPunchinByEmployeeId(long employeeId)
+        {
+            Task<IList<TimeAndAttendancePunchInView>> resultTask = Task.Run<IList<TimeAndAttendancePunchInView>>(async () => await unitOfWork.TARepository.GetTAPunchinByEmployeeId(employeeId));
+            return resultTask.Result;
+        }
+    public async Task<bool> UpdatePunchIn(TimeAndAttendancePunchIn taPunchin)
+        {
+            try
+            {
+                unitOfWork.TARepository.UpdateObject(taPunchin);
+                unitOfWork.CommitChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(GetMyMethodName(), ex);
+            }
+        }
+        public async Task<bool> DeletePunchIn(TimeAndAttendancePunchIn taPunchin)
+        {
+            try
+            {
+                bool result = await unitOfWork.TARepository.DeletePunchin(taPunchin);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(GetMyMethodName(), ex);
+            }
+        }
+    }
+}
