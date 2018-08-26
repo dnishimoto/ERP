70f5a7890793459313a912b44adbfefee37f05c1
David Nishimoto <dnishimoto@listensoftware.com>
2018-08-26 17:32:36 -0600

    customer fluent api refactoring

---
+++ AddressBook/ERP_Core2/FluentAPI/FluentCashPayment.cs
@@ -0,0 +1,31 @@
+﻿using ERP_Core2.AbstractFactory;
+using ERP_Core2.Interfaces;
+using MillenniumERP.AccountsReceivableDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP_Core2.FluentAPI
+{
+    public class FluentCustomerCashPayment : AbstractErrorHandling, IAccountsReceivableModule
+    {
+
+
+        public IGeneralLedger GeneralLedger()
+        {
+            return new FluentGeneralLedger() as IGeneralLedger;
+        }
+        public ICustomerLedger CustomerLedger()
+        {
+            return new FluentCustomerLedger() as ICustomerLedger;
+        }
+        public IAccountsReceivable AccountsReceivable()
+        {
+            return new FluentAccountsReceivable() as IAccountsReceivable;
+        }
+
+
+    }
+}
---
+++ AddressBook/ERP_Core2/Interfaces/ICustomer.cs
@@ -0,0 +1,19 @@
+﻿using MillenniumERP.CustomerDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP_Core2.Interfaces
+{
+    public interface ICustomer
+    {
+        ICustomer CreateAddressBook(CustomerView customerView);
+        ICustomer CreateCustomerLocationAddress(CustomerView customerView);
+        ICustomer CreateCustomer(CustomerView customerView);
+        ICustomer CreateCustomerEmail(CustomerView customerView);
+        ICustomer Apply();
+        ICustomerQuery Query();
+    }
+}
