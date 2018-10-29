077154a13765a61f6028a2c098ee9a72614a15a0
David Nishimoto <dnishimoto@listensoftware.com>
2018-10-29 06:30:24 -0600

    working on my angular ngmodel form

--- lssAngular2/ClientApp/src/app/personalExpense-webapi/personalExpense-webapi.component.ts
+++ lssAngular2/ClientApp/src/app/personalExpense-webapi/personalExpense-webapi.component.ts
@@ -1,17 +1,46 @@
+
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { IGeneralLedgerView } from '../interface/interfaceMod';
+//import { ViewChild, AfterViewInit } from '@angular/core';
+
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-personalExpense-webapi',
   templateUrl: './personalExpense-webapi.component.html'
 })
 
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClient
+  ],
+  declarations: [
+    AppComponent,
+    Component
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
 
 
-export class PersonalExpenseComponent {
+export class PersonalExpenseComponent  {
   public personalExpense: IGeneralLedgerView;
+  submitted = false;
 
+
+  onSubmit() {
+    alert('reached');
+    this.submitted = true;
+  }
+  newHero() { alert('reached new hero');}
 
   constructor(
     http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
