245e6b97b5f3f1e0e994197dd26b4bcb98a39a17
David Nishimoto <dnishimoto@listensoftware.com>
2019-03-07 07:13:52 -0700

    paging on ta angular

---
+++ lssAngular3/Controllers/TimeAndAttendanceController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ERP_Core2.TimeAndAttendanceDomain;
+using lssAngular2;
+using lssAngular2.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace lssAngular3.Controllers
+{
+   
+
+    [Route("api/[controller]")]
+    public class TimeAndAttendanceController : Controller
+    {
+        string _baseUrl;
+        public TimeAndAttendanceController(IOptions<AppSettings> settings)
+        {
+            AppSettings _settings = settings.Value;
+            this._baseUrl = _settings.BaseUrl;
+        }
+        // GET: api/<controller>
+        [HttpGet]
+        [Route("TAOpenPunch/{employeeId}")]
+        [ProducesResponseType(typeof(TimeAndAttendancePunchInView), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetOpenPunch(long employeeId)
+        {
+
+            DataService ds = new DataService(this._baseUrl);
+            TimeAndAttendancePunchInView view = await ds.GetAsync<TimeAndAttendancePunchInView>("api/TimeAndAttendance/TAOpenPunch/"+employeeId.ToString());
+            return Ok(view);
+        }
+        [HttpPost]
+        [Route("TAPunchout")]
+        [ProducesResponseType(typeof(TimeAndAttendancePunchInView), StatusCodes.Status200OK)]
+        public async Task<IActionResult> UpdatePunch([FromBody]TimeAndAttendanceParam param)
+        {
+            DataService ds = new DataService(this._baseUrl);
+            TimeAndAttendancePunchInView view = await ds.PostAsync<TimeAndAttendanceParam,TimeAndAttendancePunchInView>("api/TimeAndAttendance/TAPunchout",param);
+            return Ok(view);
+        }
+
+        
+        [HttpPost]
+        [Route("TAPunchin")]
+        [ProducesResponseType(typeof(TimeAndAttendancePunchInView), StatusCodes.Status200OK)]
+        public async Task<IActionResult> CreatePunchin([FromBody] TimeAndAttendanceParam param)
+        {
+            
+            DataService ds = new DataService(this._baseUrl);
+            TimeAndAttendancePunchInView view = await ds.PostAsync<TimeAndAttendanceParam, TimeAndAttendancePunchInView>("api/TimeAndAttendance/TAPunchin",param);
+            return Ok(view);
+        }
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
